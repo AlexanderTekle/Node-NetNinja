9e0ae83664bde4c034df0ae460eed401f18d5951
Alexander Tekle <tekle.alexander@gmail.com>
2017-12-27 21:14:02 -0600

    Switching to pro. Video 24 complete.

---
+++ 24-RouteParameters.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req,res){
+  res.send('homepage');
+});
+
+app.get('/contact', function(req,res){
+  res.send('this is the contact page');
+});
+
+app.get('/profile/:id', function(req,res){
+  res.send('You requested to see profile of id ' + req.params.id);
+});
+
+app.listen(3000);
